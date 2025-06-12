a6840567fc23757aea8d6a9069dfa41c99a0192e
yunwansu <yunwansu96@gmail.com>
2025-06-12 23:42:30 +0900

    add informers

--- main.go
+++ main.go
@@ -5,14 +5,19 @@ import (
 	"flag"
 	"fmt"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	memory "k8s.io/client-go/discovery/cached"
 	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/dynamic/dynamicinformer"
+	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/restmapper"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
 	"path/filepath"
+	"time"
 )
 
 func main() {
@@ -70,5 +75,37 @@ func main() {
 
 	for _, cr := range crs.Items {
 		fmt.Println(cr.GetNamespace(), "/", cr.GetName())
+	}
+
+	factory := informers.NewSharedInformerFactory(clientset, 10*time.Hour)
+	podLister := factory.Core().V1().Pods().Lister()
+
+	stopCh := make(chan struct{})
+	factory.Start(stopCh)
+	factory.WaitForCacheSync(stopCh)
+
+	pods, err := podLister.Pods("kube-system").List(labels.Everything())
+	if err != nil {
+		panic(err.Error())
+	}
+
+	for _, pod := range pods {
+		fmt.Printf("- %s/%s\n", pod.Namespace, pod.Name)
+	}
+
+	dfactory := dynamicinformer.NewDynamicSharedInformerFactory(dc, 10*time.Hour)
+	configMapInformer := dfactory.ForResource(schema.GroupVersionResource{Group: "", Version: "v1", Resource: "configmaps"})
+	configMapLister := configMapInformer.Lister()
+	dfactory.Start(stopCh)
+	dfactory.WaitForCacheSync(stopCh)
+
+	configMaps, err := configMapLister.List(labels.Everything())
+	if err != nil {
+		panic(err.Error())
+	}
+
+	for _, configMap := range configMaps {
+		u := configMap.(*unstructured.Unstructured)
+		fmt.Printf("- %s/%s\n", u.GetNamespace(), u.GetName())
 	}
 }
